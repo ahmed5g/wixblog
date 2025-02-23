cdac02c3e3fbb927f7a1b3b8032729c7d5aabb32
Ahmed <57914232+ahmed5g@users.noreply.github.com>
2025-02-23 17:45:05 +0100

    add backend project setup initialisations

---
+++ wixblog backend/src/main/java/com/tech/wixblog/WixblogApplication.java
@@ -0,0 +1,13 @@
+package com.tech.wixblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WixblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WixblogApplication.class, args);
+	}
+
+}
---
+++ wixblog backend/src/test/java/com/tech/wixblog/WixblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.wixblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WixblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
