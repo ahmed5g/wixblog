17633badc80952ef1f91bf7a5a739dba46a2b6b4
Ahmed <57914232+ahmed5g@users.noreply.github.com>
2025-04-07 17:10:57 +0100

    add user Authentication endpoint - Login

--- wixblog backend/src/main/java/com/tech/wixblog/config/SecurityConfig.java
+++ wixblog backend/src/main/java/com/tech/wixblog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.tech.wixblog.config;
 
+import com.tech.wixblog.domain.entities.User;
+import com.tech.wixblog.repositories.UserRepository;
+import com.tech.wixblog.security.BlogUserDetailsService;
+import com.tech.wixblog.security.JwtAuthenticationFilter;
+import com.tech.wixblog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,26 +12,55 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService){
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(userRepository);
+
+        String email = "user@test.com";
+        userRepository.findByEmail(email).orElseGet(() -> {
+            User newUser = User.builder()
+                    .name("Test user")
+                    .email(email)
+                    .password(passwordEncoder().encode("password"))
+                    .build();
+
+            return userRepository.save(newUser);
+        });
+        return blogUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF (enable if needed)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET,"/api/v1/category/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/post/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/tags/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated() // Secure all other endpoints
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Maintain session
-                );
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ wixblog backend/src/main/java/com/tech/wixblog/domain/dto/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.tech.wixblog.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
